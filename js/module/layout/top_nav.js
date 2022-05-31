11255fb293190cb143f7c0ef51fdae0db8f4f81c
Kenna <kenna.hwa@gmail.com>
2022-05-31 18:41:41 +0900

    Style: top_nav 스크립트 삭제 및 bottom_nav 생성
    - 카테고리명 한글화 하면서 스크립트 처리 삭제
    - padding 값 전체 삽입에서 헤더, 컨텐츠, 푸터 각각 적용되도록 변경

--- js/module/layout/top_nav.js
+++ js/module/layout/top_nav.js
@@ -1,14 +1,14 @@
 
 
-const controlCategoryName = () => {
-    const category = document.querySelectorAll(".top_nav_category_link");
-    console.log(category);
-    const korCategory = ["패션", "식품", "리빙", "뷰티", "전체보기"];
-    const arrCategory = [...category];
-    arrCategory.map((node, i)=>{
-      node.innerHTML=korCategory[i]
-    })
-  }
+// const controlCategoryName = () => {
+//     const category = document.querySelectorAll(".top_nav_category_link");
+//     console.log(category);
+//     const korCategory = ["패션", "식품", "리빙", "뷰티"];
+//     const arrCategory = [...category];
+//     arrCategory.map((node, i)=>{
+//       node.innerHTML=korCategory[i]
+//     })
+//   }
   
   
   // swiper
@@ -26,9 +26,9 @@ const controlCategoryName = () => {
   //함수 실행
   if (document.readyState == 'loading') {
     // 아직 로딩 중이므로 이벤트를 기다립니다.
-    document.addEventListener('DOMContentLoaded', controlCategoryName);
+    // document.addEventListener('DOMContentLoaded', controlCategoryName);
   } else {
     // DOM이 완성되었습니다!
-    controlCategoryName();
+    // controlCategoryName();
   }
   
