9dcacc5ad891f16d10053e08d4bdb05ef69b11f0
Kenna <kenna.hwa@gmail.com>
2022-05-31 17:50:19 +0900

    Style: header, top_nav 생성
    - header 파일 생성 및 스타일링
    
    - header 내부 top_nav 파일 생성 및 스타일링
    - top_nav 파일 스크립트 적용
    - swiper 라이브러리 삽입
    - layout 사이징
    - images 폴더 생성

---
+++ js/module/layout/top_nav.js
@@ -0,0 +1,34 @@
+
+
+const controlCategoryName = () => {
+    const category = document.querySelectorAll(".top_nav_category_link");
+    console.log(category);
+    const korCategory = ["패션", "식품", "리빙", "뷰티", "전체보기"];
+    const arrCategory = [...category];
+    arrCategory.map((node, i)=>{
+      node.innerHTML=korCategory[i]
+    })
+  }
+  
+  
+  // swiper
+  var swiper = new Swiper('.swiper', {
+      slidesPerView: 6,
+      direction: "horizontal",
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      on: {
+      },
+    });
+  
+  //함수 실행
+  if (document.readyState == 'loading') {
+    // 아직 로딩 중이므로 이벤트를 기다립니다.
+    document.addEventListener('DOMContentLoaded', controlCategoryName);
+  } else {
+    // DOM이 완성되었습니다!
+    controlCategoryName();
+  }
+  
