7436c34d2be96f2d8d7cd9af8ea765140f56f9c8
Kenna <kenna.hwa@gmail.com>
2022-06-02 15:00:03 +0900

    Feat: top_nav, bottom_nav 컴포넌트 완성
    - heade,  top_nav 완료
    - bottom_nav 완료 (링크 미설정)
    - 각 메뉴의 링크 설정 필요

--- js/module/layout/bottom_nav.js
+++ js/module/layout/bottom_nav.js
@@ -6,7 +6,25 @@
       console.log("path", page_path)
   
       const bottomControl = () => {
-      page_path === "/" ? document.documentElement.style.setProperty("--store", `url("../../images/icon/icon-store.png")`) : page_path === "/myshop/community/community_list.html" ? document.documentElement.style.setProperty("--feed", `url("../../images/icon/icon-feed.png")`) : page_path === "/myshop/wish_list.html" ? document.documentElement.style.setProperty("--like", `url("../../images/icon/icon-like.png")`) : page_path === "/product/cat.html" ? document.documentElement.style.setProperty("--category", `url("../../images/icon/icon-category.png")`) : page_path.includes('member') ? document.documentElement.style.setProperty("--user", `url("../../images/icon/icon-user.png")`) : null;
+      page_path === "/" ? 
+      (document.documentElement.style.setProperty("--store", `url("../../images/icon/icon-store.png")`),
+      document.documentElement.style.setProperty("--text-color-store", `#022CFF`)
+      ) : 
+      page_path === "/myshop/community/community_list.html" ? 
+      (document.documentElement.style.setProperty("--feed", `url("../../images/icon/icon-feed.png")`),
+      document.documentElement.style.setProperty("--text-color-feed", `#022CFF`)
+      ) : 
+      page_path === "/myshop/wish_list.html" ? 
+      (document.documentElement.style.setProperty("--like", `url("../../images/icon/icon-like.png")`),
+      document.documentElement.style.setProperty("--text-color-like", `#022CFF`)
+      ) : 
+      page_path === "/product/cat.html" ? 
+      (document.documentElement.style.setProperty("--category", `url("../../images/icon/icon-category.png")`),
+      document.documentElement.style.setProperty("--text-color-category", `#022CFF`)) : 
+      page_path.includes('member') ? 
+      (document.documentElement.style.setProperty("--user", `url("../../images/icon/icon-user.png")`),
+      document.documentElement.style.setProperty("--text-color-user", `#022CFF`)) : 
+      null;
       }
   
   
