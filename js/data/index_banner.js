0094706fe6603a3329e3b254b87293848986cb21
Kenna <kenna.hwa@gmail.com>
2022-07-01 14:42:08 +0900

    Fix:

---
+++ js/data/index_banner.js
@@ -0,0 +1,7 @@
+let bannerList = {
+    1: {
+        "url" : "/bvtIMG/banner/squarebanner_210422_3.png",
+        "link" : "/front/php/b/board_read.php?board_no=2&amp;no=604" 
+    }
+}
+return bannerList;
