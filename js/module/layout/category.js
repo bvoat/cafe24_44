8f57cd8093cfa959d5f66a7baa260319f6ff3f69
Kenna <kenna.hwa@gmail.com>
2022-07-13 17:49:25 +0900

    Fix: category 수정 배포

--- js/module/layout/category.js
+++ js/module/layout/category.js
@@ -126,11 +126,9 @@
         }
     }, 100);
 
-    $(`div[class^="middle-category"]`).click(function () {
+    $(document).on("click", `div[class^="middle-category"]`, function () {
         var click_tmp = $(this);
-        console.log('click_tmp: ', click_tmp);
         var segment_array = click_tmp.attr("class").split('-');
-        console.log('segment_array: ', segment_array);
         var last_segment = segment_array.pop();
         
         $(`div[class^="middle-category"]`).next(`.middle-small`).remove();
@@ -165,7 +163,7 @@
     
     })
     
-    $(`div[class^="middle-tag-category"]`).click(function () {
+    $(document).on("click", `div[class^="middle-tag-category"]`, function () {
         var click_tmp = $(this);
         var segment_array = click_tmp.attr("class").split('-');
         var last_segment = segment_array.pop();
