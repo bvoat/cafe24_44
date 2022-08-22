f53af11bf847c8a18521bc9ed6a708a372f3dec2
Kenna <kenna.hwa@gmail.com>
2022-08-22 11:56:28 +0900

    Fix: detail.js 내 createStamp 함수 settimeout 적용

--- js/module/product/detail.js
+++ js/module/product/detail.js
@@ -300,9 +300,9 @@ qnaControl();
 //bvt_common.js 파일에서 가격 정보 받아온 후 실행해야 함
 window.addEventListener("load", ()=>{
   if(document.querySelector(".finalprice")){
-    createStamp(createStampPcsFromPrice)
+    setTimeout(()=>{createStamp(createStampPcsFromPrice)}, 500)
   }else{
-    setTimeout(()=>{createStamp(createStampPcsFromPrice)}, 1000)
+    setTimeout(()=>{createStamp(createStampPcsFromPrice)}, 1200)
   }
 })
 //가치태그 수신
