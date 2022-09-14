5cb4307c90bd7072d2101066f73b392ee7957686
Kenna <kenna.hwa@gmail.com>
2022-09-14 16:11:44 +0900

    Fix: footer 내 비밀링크 삭제, 메인 console의 realtime 표시 삭제
    - footer a,b로 소셜 프루프 페이지 비밀 링크 연결한 것 삭제
    - realtime 콘솔에 log 표시 삭제

--- js/module/product/realtime.js
+++ js/module/product/realtime.js
@@ -10,18 +10,17 @@
     .then((response) => {
         if(response.success){
             const productList = document.querySelectorAll(".realtime_order_list>ul>li");
-            console.log('productList: ', productList);
             [...productList].forEach((product)=>{
                 response.data.forEach((data)=>{
                     if(product.dataset.productno == data.product_no){
-                        console.log("matching", product.dataset.productno, data.product_no, data.masking_name)
+                        // console.log("matching", product.dataset.productno, data.product_no, data.masking_name)
                         product.insertAdjacentHTML("afterbegin", `<div class="purchaser">보터<span class="voter_name">${data.masking_name}</span>님</div>`)
                     }
                 });
 
             })
         }else{
-            console.log("response", response)
+            // console.log("response", response)
             product.insertAdjacentHTML("afterbegin", `<div class="purchaser">보터<span class="voter_name">익명</span>님</div>`)
         }
     })
