8a1e1ed089e043eb21d37f98f9233cac7f4349fe
Kenna <kenna.hwa@gmail.com>
2022-09-18 19:26:42 +0900

    Fix: recommend list fontsize 오류 전체 수정 중

--- js/module/product/list.js
+++ js/module/product/list.js
@@ -33,7 +33,7 @@ window.addEventListener("DOMContentLoaded",makeHeadingTwo(category, description)
 /* category description 추가 */
 /* 추천 카테고리 슬라이드 */
 var swiper = new Swiper("#productListrecommendList", {
-  slidesPerView: 2,
+  slidesPerView: "auto",
   spaceBetween: 14,
   pagination: {
     el: ".product_list_pagination",
