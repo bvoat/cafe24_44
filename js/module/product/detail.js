1854c86117e6910bab42a9655f148aadb3dd90b5
Kenna <kenna.hwa@gmail.com>
2023-09-09 12:07:02 +0900

    Fix: detail 수정

--- js/module/product/detail.js
+++ js/module/product/detail.js
@@ -260,7 +260,7 @@ const review_num_insert = (review_item_length) => {
 
 //user_name 수정
 const user_name_arr = document.querySelectorAll(".user_name");
-user_name_arr.forEach(name => name.innerHTML == "비보트" ? name.innerHTML = "비회원 구매자" : null);
+user_name_arr.forEach(name => name.innerHTML === "비보트" ? name.innerHTML = "비회원 구매자" : null);
  
 /* 최종 함수 실행 */
 
